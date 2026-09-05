d3bc6764d0579fb243655e9b696c28cf3e8e20c6
universalrp2003 <universalrp2003@gmail.com>
2026-09-05 18:26:37 +0530

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Root-level build file. Rename to "build.gradle.kts" and place in the project root.
+plugins {
+    id("com.android.application") version "8.13.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
+}
